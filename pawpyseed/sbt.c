4e1b762c13878f87f227a839184ef6791913632c
kylebystrom <kylebystrom@berkeley.edu>
2018-01-29 21:31:43 -0800

    finish sbt

--- pawpyseed/sbt.c
+++ pawpyseed/sbt.c
@@ -12,7 +12,20 @@
 #define c 0.262465831
 #define PI M_PI
 
+typedef struct sbt_setup {
+	double kmin;
+	double kappamin;
+	double rmin;
+	double rhomin;
+	double rho; //drho == dkappa
+	double dt;
+	double N;
+	double complex** mult_table;
+} sbt_desciptor_t;
+
 double complex* spherical_bessel_transform_setup(double encut, double enbuf, int lmax, int N, double* r) {
+
+	sbt_desciptor_t* descriptor = (sbt_desciptor_t*) malloc(sizeof(sbt_desciptor_t));
 
 	double kmin = pow((encut+enbuf) * c, 0.5) * exp(-(N-1) * drho);
 	double kappamin = log(kmin);
@@ -33,11 +46,42 @@ double complex* spherical_bessel_transform_setup(double encut, double enbuf, int
 			-sin(7*phi)/(1680*pow(rad,7));
 		for (int j = 0; j < 10; j++)
 			phi1 += atan((2*t)/(2*j-1));
-		rad = pow()
+		
+		phi2 = -atan(sinh(PI*t/1)/cosh(PI*t/2));
+		phi = phi1 + phi2 + phi3;
+		mult_table[0][i] = pow(PI/2, 0.5) * cexp(I * phi) / N;
+		if (it == 1) mult_table[0][it] = 0.5 * mult_table[0][it];
+		phi = -phi2-atan(2*t);
+		mult_table[1][i] = cexp(2 * I * phi) * mult_table[0][i];
+		for (int l = 2; l <= lmax) {
+			phi = -atan(2*t/(2*l+1));
+			mult_table[i][l] = exp(2* I * phi) * mult_table[i][l-2];
+		}
 	}
+
+	descriptor->kmin = kmin;
+	descriptor->kappamin = kappamin;
+	descriptor->rmin = rmin;
+	descriptor->rhomin = rhomin;
+	descriptor->drho = drho;
+	descriptor->dt = dt;
+	descriptor->N = N;
+	descriptor->mult_table = mult_table;
+
 }
 
-double complex* wave_spherical_bessel_transform(double* r, double* f, double maxE, int N, int l, int m) {
+double complex* wave_spherical_bessel_transform(sbt_desciptor_t* d,
+	double* r, double* f, int l, int m) {
+
+	double kmin = d->kmin;
+	double kappamin = d->kappamin;
+	double rmin = d->rmin;
+	double rhomin = d->rhomin;
+	double drho = d->drho;
+	double dt = d->dt;
+	double N = d->N;
+	double complex** M = d->mult_table;
+
 	MKL_Complex16* x = mkl_malloc(N*sizeof(MKL_Complex16), 64);
 
 	DFTI_DESCRIPTOR_HANDLE handle = 0;
@@ -51,12 +95,6 @@ double complex* wave_spherical_bessel_transform(double* r, double* f, double max
 	status = DftiCommitDescriptor(handle);
 	status = DftiComputeBackward(handle, x);
 
-	double drho = log(r[1] / r[0]);
-	double rhomin = log(r[0]);
-	double rmin = r[0];
-	
-	double t[N];
-	double dt = 2 * PI / N / drho;
 	double phase = 0;
 	for (int i = 0; i < N; i++) {
 		t[i] = dt * i;
@@ -68,10 +106,9 @@ double complex* wave_spherical_bessel_transform(double* r, double* f, double max
 	double rp, ip;
 	status = DftiComputeBackward(handle, x);
 	for (int n = 0; n < N; n++) {
-		rp = x[n].real * creal(M(l, t[n])) - x[n].imag * cimag(M(l, t[n]));
-		ip = x[n].imag * creal(M(l, t[n])) + x[n].real * cimag(M(l, t[n]));
+		rp = x[n].real * creal(M[l][n]) - x[n].imag * cimag(M[l][n]);
+		ip = x[n].imag * creal(M[l][n]) + x[n].real * cimag(M[l][n]);
 		phase = I * (kappamin + rhomin) * n * dt;
-		M(l, t[n]);
 		x[n].real = rp * cos(phase) - ip * sin(phase);
 		x[n].imag = ip * cos(phase) + rp * sin(phase);
 		if (n > N/2) {

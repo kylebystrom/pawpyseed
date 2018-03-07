a6b66be55091740b8803afb5460793f7c9bac8b7
Kyle William Bystrom <kylebystrom@berkeley.edu>
2018-03-07 14:13:34 -0800

    package setup

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+import os, subprocess
+import sys
 
-cargs = ['-std=c11', '-lmkl_rt', '-fopenmp', '-lpthread', '-ldl', '-lm', '-O3', '-fPIC', '-Wall']
+os.environ["CC"] = "icc"
+os.environ["CXX"] = "icc"
 
-setup(name='PAWpySeed',
+srcfiles = ['fft', 'projector', 'pseudoprojector', 'quadrature', 'radial', 'reader', 'sbt', 'tests', 'utils']
+cfiles = [f+'.c' for f in srcfiles]
+hfiles = [f+'.h' for f in srcfiles]
+
+setup(name='pawpyseed',
 	version='0.0.1',
 	description='Parallelized DFT wavefunction analysis utilities',
 	author='Kyle Bystrom',
 	author_email='kylebystrom@berkeley.edu',
 	packages=['pawpyseed'],
-	ext_modules=[Extension('pawpy', ['pseudoprojector.c', 'projector.c', 'fft.c', 'quadrature.c', \
-                'reader.c', 'utils.c', 'radial.c', 'tests.c', 'sbt.c'],
-                library_dirs=['${MKLROOT}/lib/intel64'],
-                include_dirs=['${MKLROOT}/include'],
-                extra_compile_args=cargs,
-                extra_link_args=cargs)]
-	)
+	package_data={'pawpyseed': cfiles+hfiles+['Makefile', 'pawpy.so']},
+	scripts=['pawpy_example.py']
+)
+
+if len(sys.argv) > 1 and sys.argv[1] == 'build':
+	currdir = os.getcwd()
+	os.chdir(os.path.join(currdir,'build/lib/pawpyseed/'))
+	subprocess.call('make projector'.split())
+	os.chdir(currdir)
+

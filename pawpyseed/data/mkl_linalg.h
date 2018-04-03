124d6b5154f8857cb0de268a8c7fbdc6121eeb46
kylebystrom <kylebystrom@berkeley.edu>
2018-04-03 15:10:40 -0700

    more thorough C docs

--- pawpyseed/data/mkl_linalg.h
+++ pawpyseed/data/mkl_linalg.h
@@ -1,3 +1,7 @@
+/** \file
+Linear algebra routines performed by interacing with the Intel Math Kernel Library
+*/
+
 #ifndef FFT_H
 #define FFT_H
 #include <mkl.h>
@@ -5,12 +9,26 @@
 
 #define fft_complex MKL_Complex16
 
+/**
+DEPRECATED, DO NOT USE
+*/
 void trilinear_interpolate_values(MKL_Complex16* x, double* frac, int* fftg, double complex* values);
 
+/**
+Utility allocation function to allocate memory for an MKL complex double.
+*/
 MKL_Complex16* fft_calloc(int num_items, int item_size);
 
+/**
+Utility function to multiply a complex MKL double and a complex C double.
+*/
 double complex fft_mult(int i, MKL_Complex16* x, double complex y);
 
+/**
+Uses the 3D fast fourier transform to calculate the wavefunction
+defined by plane-wave coefficients Cs in real space. These 
+values get stored in x.
+*/
 void fft3d(MKL_Complex16* x, int* G_bounds, double* lattice,
 	double* kpt, int* Gs, float complex* Cs, int num_waves, int* fftg);
 

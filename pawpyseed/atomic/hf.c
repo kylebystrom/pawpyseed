2b0d16e4e5d1f2a0aa1cb41573957cc5829b5ca9
kylebystrom <kylebystrom@berkeley.edu>
2018-04-03 14:16:50 -0700

    rough draft of the radially restricted hf code

--- pawpyseed/atomic/hf.c
+++ pawpyseed/atomic/hf.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <mkl.h>
+#include "condon_shortley.h"
 
 typedef radial_set {
 	int X; ///< number of radial functions;
@@ -16,7 +17,6 @@ typedef struct radial_set {
 	double*** ee; ///< electron-electron repulsion terms 4x(XxX)x(XxX) 0 2 4 6
 	double* es; ///< (X)
 	double** Ps; ///< (L)x(XxX)
-	double* r; ///< (N)
 	double*** yks; ///< (XxX)x(4)x(N) 0 2 4 6
 	double** bfs;
 	double* DM; ///< density matrix (XxX)
@@ -30,11 +30,28 @@ typedef struct awf {
 	int XT; ///< L*X
 	int N; ///< grid size
 	double*** yks; ///< (XTxXT)x(7)x(N), k = 0 1 2 3 4 5 6, indexed by (l1*X+n1)*XT+(l2*X_n2),l2,k,rindex
-	double**** J; ///< (L)x(L)x(XxX)x(XxX) indexed by l1,l2,n1a*X+n1b,n2a*X+n2b <n1al1,n2al2||n1bl1,n2bl2>
-	double**** K; ///< (L)x(L)x(XxX)x(XxX) indexed by l1,l2,n1a*X+n1b,n2a*X+n2b <n1al1,n2al2||n2bl2,n1bl1>
+	double***** J; ///< (7)x(L)x(L)x(XxX)x(XxX) indexed by l1,l2,n1a*X+n1b,n2a*X+n2b <n1al1,n2al2||n1bl1,n2bl2>
+	double***** K; ///< (7)x(L)x(L)x(XxX)x(XxX) indexed by l1,l2,n1a*X+n1b,n2a*X+n2b <n1al1,n2al2||n2bl2,n1bl1>
 	radial_set_t* wfs; ///< L radial_set_t wavefunctions
+	double* r;
 	double E; ///< total energy
 } awf_t;
+
+double laguerre(double x, int n, int alpha) {
+	double l0 = 1;
+	if (n == 0) return l0;
+	double l1 = 1 + alpha - x;
+	double lcurr = l1;
+	for (int k = 1; k < n; k++) {
+		l1 = lcurr;
+		lcurr = ((2*k+1+alpha-x) * l1 - (k+alpha) * l0) / (k+1);
+		l0 = l1;
+	}
+}
+
+double hradial() {
+	pow(4.0/n/n/n * fac(n-l-1)/fac(n+l), 0.5) * exp(-r/2/n) * pow(r/n, l), * laguerre(r, n-l-1, 2*l+1);
+}
 
 double get_yk(double*** yks, int X, int XT, int l1, int n1, int l2, int n2, int k, int rindex) {
 	return yks[(l1*X+n1)*XT+(l2*X_n2)][k][rindex];
@@ -44,12 +61,12 @@ void set_yk(double*** yks, double* nums, int X, int XT, int l1, int n1, int l2, 
 	yks[(l1*X+n1)*XT+(l2*X_n2)][k][rindex] = nums;
 }
 
-double get_coul(double**** J_or_K, int X, int l1, int n1a, int n1b, int l2, int n2a, int n2b) {
-	return J_or_K[l1][l2][n1a*X+n1b][n2a*X+n2b];
+double get_coul(double**** J_or_K, int k, int X, int l1, int n1a, int n1b, int l2, int n2a, int n2b) {
+	return J_or_K[k][l1][l2][n1a*X+n1b][n2a*X+n2b];
 }
 
-void set_coul(double**** J_or_K, double num, int X, int l1, int n1a, int n1b, int l2, int n2a, int n2b) {
-	J_or_K[l1][l2][n1a*X+n1b][n2a*X+n2b] =  num;
+void set_coul(double**** J_or_K, double num, int k, int X, int l1, int n1a, int n1b, int l2, int n2a, int n2b) {
+	J_or_K[k][l1][l2][n1a*X+n1b][n2a*X+n2b] =  num;
 }
 
 double* discrete_sp_hamiltonian(int Z, int l, int size, double* r) {
@@ -88,7 +105,12 @@ awf_t* construct_basis(int Z, int N, int maxN, int maxL, double* r) {
 			splines[n-1] = spline_coeff(r, bf, N);
 			h[X*n-X+n-1] = -0.5*Z*Z/(n+l)/(n+l);
 		}
-		wfs[l].bs = bfs;
+		wfs[l].bfs = bfs;
+		wfs[l].l = l;
+		wfs[l].v = 0;
+		wfs[l].X = X;
+		wfs[l].h = h;
+		wfs[l].occs = occs;
 	}
 	double*** yks = (double***) malloc(XT*XT * sizeof(double**));
 	int b1, b2, n1, n2, l1, l2;
@@ -105,79 +127,54 @@ awf_t* construct_basis(int Z, int N, int maxN, int maxL, double* r) {
 			}
 		}
 	}
-	double**** J = (double****) malloc(L * sizeof(double***));
-	double**** K = (double****) malloc(L * sizeof(double***));
-	for (int l1 = 0; l1 < L; l1++) {
-		for (int l2 = 0; l2 < L; l2++) {
-			for (int n1a = 0; n1a < X; n1a++) {
-				for (int n1b = 0; n1b < X; n1b++) {
-					for (int n2a = 0; n2a < X; n2a++) {
-						for (int n2b = 0; n2b < X; n2b++) {
-							double* integrandJ = (double*) malloc(N * sizeof(double));
-							double* integrandK = (double*) malloc(N * sizeof(double));
-							for (int rnum = 0; rnum < N; rnum++) {
-								integrandJ[rnum] = get_yk(yks, X, XT, l2, n2a, l2, n2b, k, rnum)
-									* wfs[l1].bfs[n1a] * wfs[l1].bfs[n1a] / r[rnum];
-								integrandK[rnum] = get_yk(yks, X, XT, l2, n2a, l1, n1b, k, rnum)
-									* wfs[l1].bfs[n1a] * wfs[l2].bfs[n2b] / r[rnum];
+	double***** J = (double*****) malloc(7 * sizeof(double****));
+	double***** K = (double*****) malloc(7 * sizeof(double****));
+	for (int k = 0; k < 7; k++) {
+		J[k] = (double****) malloc(L * sizeof(double***));
+		K[k] = (double****) malloc(L * sizeof(double***));
+		for (int l1 = 0; l1 < L; l1++) {
+			J[k][l1] = (double***) malloc(L * sizeof(double**));
+			K[k][l1] = (double***) malloc(L * sizeof(double**));
+			for (int l2 = 0; l2 < L; l2++) {
+				J[k][l1][l2] = (double**) malloc(X * X * sizeof(double*));
+				K[k][l1][l2] = (double**) malloc(X * X * sizeof(double*));
+				for (int n1a = 0; n1a < X; n1a++) {
+					for (int n1b = 0; n1b < X; n1b++) {
+						J[k][l1][l2][n1a*X+n1b] = (double*) malloc(X * X * sizeof(double));
+						K[k][l1][l2][n1a*X+n1b] = (double*) malloc(X * X * sizeof(double));
+						for (int n2a = 0; n2a < X; n2a++) {
+							for (int n2b = 0; n2b < X; n2b++) {
+								double* integrandJ = (double*) malloc(N * sizeof(double));
+								double* integrandK = (double*) malloc(N * sizeof(double));
+								for (int rnum = 0; rnum < N; rnum++) {
+									integrandJ[rnum] = get_yk(yks, X, XT, l2, n2a, l2, n2b, k, rnum)
+										* wfs[l1].bfs[n1a] * wfs[l1].bfs[n1a] / r[rnum];
+									integrandK[rnum] = get_yk(yks, X, XT, l2, n2a, l1, n1b, k, rnum)
+										* wfs[l1].bfs[n1a] * wfs[l2].bfs[n2b] / r[rnum];
+								}
+								double** splineJ = spline_coeff(r, integrandJ, N);
+								set_coul(J, spline_integral(r, integrandJ, splineJ, N),
+									k, X, XT, l1, n1a, n1b, l2, n2a, n2b);
+								double** splineK = spline_coeff(r, integrandK, N);
+								set_coul(K, spline_integral(r, integrandK, splineK, N)
+									* pow((4*l1+2)*(4*l2+2), -0.5) * cg_coeff[k][l1][l2][l1][l2],
+									k, X, XT, l1, n1a, n1b, l2, n2a, n2b);
+								free(splineJ[0]);
+								free(splineJ[1]);
+								free(splineJ[2]);
+								free(splineJ);
+								free(integrandJ);
+								free(splineK[0]);
+								free(splineK[1]);
+								free(splineK[2]);
+								free(splineK);
+								free(integrandK);
 							}
-							double** splineJ = spline_coeff(r, integrandJ, N);
-							set_coul(J, spline_integral(r, integrandJ, splineJ, N),
-								X, XT, l1, n1a, n1b, l2, n2a, n2b);
-							double** splineK = spline_coeff(r, integrandK, N);
-							set_coul(K, spline_integral(r, integrandK, splineK, N),
-								X, XT, l1, n1a, n1b, l2, n2a, n2b);
-							free(splineJ[0]);
-							free(splineJ[1]);
-							free(splineJ[2]);
-							free(splineJ);
-							free(integrandJ);
-							free(splineK[0]);
-							free(splineK[1]);
-							free(splineK[2]);
-							free(splineK);
-							free(integrandK);
 						}
 					}
 				}
 			}
 		}
-	}
-	for (int l = 0; l < L; l++) {
-		double*** ees = (double***) malloc(4 * sizeof(double**));
-		for (int knum = 0; knum < 4; knum++) {
-			ees[knum] = (double**) malloc(X*X * sizeof(double*));
-			int b3, b4;
-			for (int i = 0; i < X * X; i++) {
-				ees[knum][i] = (double*) malloc(X*X * sizeof(double));
-				b1 = i / X;
-				b2 = i % X;
-				for (int j = 0; j < XT * XT; j++) {
-					b3 = j / XT;
-					b4 = j % XT;
-					double* integrand = (double*) malloc(N * sizeof(double));
-					for (int rnum = 0; rnum < N; rnum++) {
-						integrand[rnum] = yks[j][knum][rnum] * bfs[b1] * bfs[b2] / r[rnum];
-					}
-					double** spline = spline_coeff(r, integrand, N);
-					ees[knum][i][j] = spline_integral(r, integrand, spline, N);
-					free(spline[0]);
-					free(spline[1]);
-					free(spline[2]);
-					free(spline);
-					free(integrand);
-				}
-			}
-		}
-
-		wfs[l].l = l;
-		wfs[l].v = 0;
-		wfs[l].X = X;
-		wfs[l].ee = ees;
-		wfs[l].h = h;
-		wfs[l].occs = occs;
-		wfs[l].yks = yks;
-		wfs[l].r = r;
 	}
 
 	awf_t* wf = (awf_t*) malloc(sizeof(awf_t));
@@ -185,7 +182,11 @@ awf_t* construct_basis(int Z, int N, int maxN, int maxL, double* r) {
 	wf->L = L;
 	wf->X = X;
 	wf->N = N;
+	wf->J = J;
+	wf->K = K;
 	wf->Z = 1;
+	wf->yks = yks;
+	wf->r = r;
 	double E = 0;
 
 	return wf;
@@ -260,31 +261,47 @@ void make_density_matrix(radial_set_t* wf) {
 	free(temp);
 }
 
-LAPACKE_dsyevd(LAPACKE_ROW_MAJOR, 'V', 'U', X, hamiltonian, X, es);
-
 void solve(awf_t* wf, int maxsteps) {
 
 	int X = wf->X;
 	for (int step = 0; step < maxsteps; step++) {
 		for (int l = 0; l < wf->L; l++) {
 
-			radial_set_t* rwf = wf->wfs[l];
+			radial_set_t rwf = wf->wfs[l];
 
 			double* hamiltonian = (double*) malloc(X * X * sizeof(double));
 
-			make_density_matrix(rwf->Ps);
+			double* es = rwf.es;
+			/*make_density_matrix(rwf.Ps);
+			for (int p = 0; p < X; P++) {
+				if (wf->wfs[l].occs[p] != 0) {
+					double coul = 0;
+					for (int mu = 0; mu < X; mu++) {
+						for (int nu = 0; nu < X; nu++) {
+							coul += (1-wf->wfs[l].occs[p]) * Ps[X*mu+p] * Ps[X*nu+p]
+								* get_coul(wf->J, 0, X, l, mu, nu, l, mu, nu);
+							for (int k = 2; k < 7; k+=2) {
+								coul -= get_coul(wf->K, k, X, l, mu, nu, l, mu, nu);
+							}
+						}
+					}
+				}
+			}*/
 			double J, K;
 			for (int mu = 0; mu < X; mu++) {
 				for (int nu = 0; nu < X; nu++) {
-					hamiltonian[X*mu+nu] = rwf->h[X*mu+nu];
+					hamiltonian[X*mu+nu] = rwf.h[X*mu+nu];
 					for (int lj = 0; lj < wf->L; lj++) {
 						double* DM = wf->wfs[lj].DM;
-						double** ee = wf->wfs[lj].ee[lj];
+						double** ee = wf->wfs[l].ee[lj];
 						for (int lambda = 0; lambda < X; lambda++) {
 							for (int sigma = 0; sigma < X; sigma++) {
-								J = ee[mu*X+nu][lambda*X+sigma];
 								hamiltonian[X*mu+nu] += DM[lambda*X+sigma] *
-									();
+									get_coul(wf->J, 0, X, l, mu, nu, lj, lambda, sigma);
+								for (int k = 0; k < 7; k++) {
+									hamiltonian[X*mu+nu] -= DM[lambda*X+sigma] *
+										get_coul(wf->K, k, X, l, mu, nu, lj, lambda, sigma);
+								}
 							}
 						}
 					}
@@ -299,6 +316,9 @@ void solve(awf_t* wf, int maxsteps) {
 					}
 				}
 			}
+			LAPACKE_dsyevd(LAPACKE_ROW_MAJOR, 'V', 'U', X, hamiltonian, X, rwf.es);
+			free(rwf.Ps);
+			rwf.Ps = hamiltonian;
 		}
 	}
 }

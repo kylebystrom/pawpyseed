800cd459cb4357a4fe64b676f98f6e3516ae76fe
kylebystrom <kylebystrom@berkeley.edu>
2018-04-06 12:07:23 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ setup(name='pawpyseed',
 	description='Parallelized DFT wavefunction analysis utilities',
 	author='Kyle Bystrom',
 	author_email='kylebystrom@berkeley.edu',
-	packages=['pawpyseed', 'pawpyseed.data', 'pawpyseed.analysis'],
-	package_data={'pawpyseed.data': cfiles+hfiles+['Makefile', 'pawpy.so']},
+	packages=['pawpyseed', 'pawpyseed.core', 'pawpyseed.analysis'],
+	package_data={'pawpyseed.core': cfiles+hfiles+['Makefile', 'pawpy.so']},
 #	scripts=['scripts/pawpy_example.py']
 )
 
 if len(sys.argv) > 1 and sys.argv[1] == 'build':
 	currdir = os.getcwd()
-	os.chdir(os.path.join(currdir,'build/lib/pawpyseed/data'))
+	os.chdir(os.path.join(currdir,'build/lib/pawpyseed/core'))
 	shutil.copyfile('mkl_linalg.c', 'linalg.c')
 	shutil.copyfile('mkl_linalg.h', 'linalg.h')
 	subprocess.call('make pawpy_icc'.split())

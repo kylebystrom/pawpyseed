6179fab81a7a026c72a44660ebc71b578269e847
kylebystrom <kylebystrom@berkeley.edu>
2018-04-18 22:04:26 -0700

    update quadrature

--- pawpyseed/core/quadrature.py
+++ pawpyseed/core/quadrature.py
@@ -1,30 +1,32 @@
 import numpy as np
 
+MAXSIZE = 100
+
 f = open("quadrature.c", "w")
 
 f.write('#include "quadrature.h"\n\n')
 
 x = []
 
-for i in range(3, 30):
+for i in range(3, MAXSIZE+1):
 	x.append( np.polynomial.legendre.leggauss(i) )
 
-f.write('double QUADRATURE_POINTS[27][30] = {\n')
+f.write('double QUADRATURE_POINTS[%d][%d] = {\n' % (MAXSIZE-2, MAXSIZE+1))
 
-for i in range(27):
+for i in range(MAXSIZE-2):
 	print(i)
 	currstr = str(x[i][0].tolist()).replace("[", "{").replace("]", "}")
 	f.write("\t" + currstr)
-	if i != 29: f.write(',\n')
+	if i != MAXSIZE: f.write(',\n')
 
 f.write('}\n\n')
 
-f.write('double QUADRATURE_WEIGHTS[27][30] = {\n')
+f.write('double QUADRATURE_WEIGHTS[%d][%d] = {\n' % (MAXSIZE-2, MAXSIZE+1))
 
-for i in range(27):
+for i in range(MAXSIZE-2):
 	currstr = str(x[i][1].tolist()).replace("[", "{").replace("]", "}")
 	f.write("\t" + currstr)
-	if i != 29: f.write(',\n')
+	if i != MAXSIZE: f.write(',\n')
 
 f.write('}\n')
 

64004836671ed99d7190d8c0abb7eb9628c119a5
kylebystrom <kylebystrom@berkeley.edu>
2018-04-18 21:44:12 -0700

    radial.c precision improvements

--- pawpyseed/core/radial.c
+++ pawpyseed/core/radial.c
@@ -8,9 +8,6 @@
 #include "radial.h"
 
 #define PI 3.14159265358979323846
-#define NUM_RADIAL_SUBSTEPS 3
-#define NUM_THETA 29
-#define NUM_PHI 60
 
 double complex offsite_wave_overlap(double* dcoord, double* r1, double* f1, double** spline1, int size1,
 	double* r2, double* f2, double** spline2, int size2,
@@ -19,23 +16,29 @@ double complex offsite_wave_overlap(double* dcoord, double* r1, double* f1, doub
 	double temp[3] = {0,0,0};
 	double r1max = r1[size1-1];
 	double r2max = r2[size2-1];
-	double dphi = 2 * PI / NUM_PHI;
+	double dphi = 0;
 	double THETA, PHI, R1, R2, costheta, dcostheta, sintheta, phi, integral = 0;
 	double complex F1, F2;
+	int NUM_RADIAL_SUBSTEPS, NUM_PHI, NUM_THETA = 0;
 
 	//loop over 1st coord
 	double dr = 0;
-	double* costhetas = QUADRATURE_POINTS[NUM_THETA-3];
-	double* dcosthetas = QUADRATURE_WEIGHTS[NUM_THETA-3];
 
 	for (int rstep = 0; rstep < size1-1; rstep++) {
-		dr = (r1[rstep+1] - r1[rstep]) / NUM_RADIAL_SUBSTEPS;
+		dr = (r1[rstep+1] - r1[rstep]);
+		NUM_RADIAL_SUBSTEPS = max(1, (int) (dr/0.01));
+		dr /= NUM_RADIAL_SUBSTEPS;
+		NUM_THETA = min(100, max(3, (int) (r1[rstep] * PI / 0.02)));
+		double* costhetas = QUADRATURE_POINTS[NUM_THETA-3];
+		double* dcosthetas = QUADRATURE_WEIGHTS[NUM_THETA-3];
 		for (int substep = 0; substep < NUM_RADIAL_SUBSTEPS; substep++) {
 			R1 = r1[rstep] + (substep+0.5)*dr;
 			for (int thetastep = 0; thetastep < NUM_THETA; thetastep++) {
 				costheta = costhetas[thetastep];
 				dcostheta = dcosthetas[thetastep];
 				sintheta = pow(1 - pow(costheta, 2), 0.5);
+				NUM_PHI = max((int) ((NUM_THETA+1)*2*sintheta), 6);
+				dphi = 2 * PI / NUM_PHI;
 				for (int phistep = 0; phistep < NUM_PHI; phistep++) {
 					phi = phistep * dphi;
 					temp[0] = R1 * sintheta * cos(phi) - dcoord[0];
@@ -59,12 +62,19 @@ double complex offsite_wave_overlap(double* dcoord, double* r1, double* f1, doub
 
 	for (int rstep = 0; rstep < size2-1; rstep++) {
 		dr = (r2[rstep+1] - r2[rstep]) / NUM_RADIAL_SUBSTEPS;
+		NUM_RADIAL_SUBSTEPS = max(1, (int) (dr/0.01));
+		dr /= NUM_RADIAL_SUBSTEPS;
+		NUM_THETA = min(100, max(3, (int) (r2[rstep] * PI / 0.02)));
+		double* costhetas = QUADRATURE_POINTS[NUM_THETA-3];
+		double* dcosthetas = QUADRATURE_WEIGHTS[NUM_THETA-3];
 		for (int substep = 0; substep < NUM_RADIAL_SUBSTEPS; substep++) {
 			R2 = r2[rstep] + (substep+0.5)*dr;
 			for (int thetastep = 0; thetastep < NUM_THETA; thetastep++) {
 				costheta = costhetas[thetastep];
 				dcostheta = dcosthetas[thetastep];
 				sintheta = pow(1 - pow(costheta, 2), 0.5);
+				NUM_PHI = max((int) ((NUM_THETA+1)*2*sintheta), 6);
+				dphi = 2 * PI / NUM_PHI;
 				for (int phistep = 0; phistep < NUM_PHI; phistep++) {
 					phi = phistep * dphi;
 					temp[0] = R2 * sintheta * cos(phi) + dcoord[0];
@@ -77,7 +87,7 @@ double complex offsite_wave_overlap(double* dcoord, double* r1, double* f1, doub
 						else PHI = acos(temp[0] / pow(temp[0]*temp[0] + temp[1]*temp[1], 0.5));
 						if (temp[1] < 0) PHI = 2*PI - PHI;
 						F1 = wave_interpolate(R1, size1, r1, f1, spline1) * Ylm(l1, m1, THETA, PHI);
-						F2 = (f2[rstep] + f2[rstep]) / 2 * Ylm2(l2, m2, costheta, phi);
+						F2 = wave_interpolate(R2, size2, r2, f2, spline2) * Ylm2(l2, m2, costheta, phi);
 						integral += F1 * conj(F2) * R2 / R1 * dr * dcostheta * dphi;
 					}
 				}

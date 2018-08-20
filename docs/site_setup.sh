255b980636cda60495d4610e3f92dd667268c369
kylebystrom <kylebystrom@berkeley.edu>
2018-08-20 08:36:48 -0700

    website rearrangement

--- docs/site_setup.sh
+++ docs/site_setup.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 doxygen dox.config
-mv html/search/* search
-mv html/*.* .
-rm -r html
-#python colors.py doxygen.css
-#python colors.py tabs.css
-#python colors.py search/search.css
+mv html documentation

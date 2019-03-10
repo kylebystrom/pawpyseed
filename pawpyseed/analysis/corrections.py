fc386c3a7199ed272540f5b42c0f910e3d064709
Kyle William Bystrom <kylebystrom@berkeley.edu>
2019-03-10 13:10:39 -0700

    fixes to corrections.py

--- pawpyseed/analysis/corrections.py
+++ pawpyseed/analysis/corrections.py
@@ -81,7 +81,11 @@ class DelocalizedStatePerturbationCorrection(DefectCorrection):
 
 		defect_path = d.parameters["path"]
 
-		spp = SingleParticlePlotter(d.parameters["defect_ks_delocal_data"])
+		local_bands = d.parameters["defect_ks_delocal_data"]["localized_band_indices"]
+		contain_nums = d.parameters["defect_ks_delocal_data"]["contain_nums"]
+		local_dat = {}
+		for spin in local_bands:
+			local_dat[spin] = dict(zip(local_bands[spin], contain_nums[spin]))
 
 		mb = True
 		defect = BulkCharacter.from_yaml(filename).data
@@ -121,15 +125,23 @@ class DelocalizedStatePerturbationCorrection(DefectCorrection):
 	
 		num_vbm = 0	
 		for band in defect.keys():
-			if band in spp.localized_bands:
-				print('LOCALIZED BAND', band)
-				continue
+			#if band in localized_bands[spi]:
+			#	print('LOCALIZED BAND', band)
+			#	continue
 			proj_amounts[band] = {}
 			for spin in range(nspin):
+				if band in local_dat[spin]:
+					print('LOCALIZED BAND', band, spin, local_dat[spin][band])
+					x = local_dat[spin][band]
+					frac = (1+np.exp(32*(x-0.9)))**-1
+					print(frac)
+				else:
+					frac = 1
 				band_occ = np.sum(occs[band*nwk*nspin+spin*nwk : band*nwk*nspin+(spin+1)*nwk] * weights)
 				proj_amounts[band][spin] = (defect[band][0][spin], defect[band][1][spin])
 				corr_term = (proj_amounts[band][spin][0] * (hybrid_vbm - bulk_vbm) \
 					+ proj_amounts[band][spin][1] * (hybrid_cbm - bulk_cbm))
+				corr_term *= frac
 				new_en = np.mean(ens[band][spin::spin+1]) + corr_term
 				if new_en < hybrid_vbm + potalign:
 					num_vbm += band_occ

e01e5f394fca34c50ffceb2c62e094cf22176e64
kylebystrom <kylebystrom@berkeley.edu>
2019-03-22 08:52:02 -0700

    change correction to shift all levels that are in HSE gap before shifting

--- pawpyseed/analysis/corrections.py
+++ pawpyseed/analysis/corrections.py
@@ -55,12 +55,13 @@ class PerturbationCorrection(DefectCorrection):
 				proj_amounts[band][spin] = (defect[band][0][spin], defect[band][1][spin])
 				corr_term = (proj_amounts[band][spin][0] * (hybrid_vbm - bulk_vbm) \
 					+ proj_amounts[band][spin][1] * (hybrid_cbm - bulk_cbm))
-				new_en = np.mean(ens[band][spin*nwk:(spin+1)*nwk]) + corr_term
-				if new_en < hybrid_vbm + potalign:
+				en = np.sum(ens[band][spin*nwk:(spin+1)*nwk] * weights)
+				new_en = en + corr_term
+				if en < hybrid_vbm + potalign:
 					num_vbm += band_occ
 					print('HOLE IN VB', band, spin, band_occ*(hybrid_vbm-bulk_vbm))
 					corr += band_occ * (hybrid_vbm - bulk_vbm)
-				elif new_en > hybrid_cbm + potalign:
+				elif en > hybrid_cbm + potalign:
 					print('ELEC IN CB', band, spin, band_occ*(hybrid_cbm-bulk_cbm))
 					corr += band_occ * (hybrid_cbm - bulk_cbm)
 				else:
@@ -209,14 +210,14 @@ class DelocalizedStatePerturbationCorrection2(DefectCorrection):
 				if band in local_dat[spin]:
 					print('LOCALIZED BAND', band, spin, local_dat[spin][band])
 					x = local_dat[spin][band]
-					frac = (1+np.exp(10*(x-0.75)))**-1
+					#frac = (1+np.exp(10*(x-0.75)))**-1
 					frac = min(1, max(1 - x, 0))
 					print(frac)
 				else:
 					frac = 1
 				band_occ = np.sum(occs[band*nwk*nspin+spin*nwk : band*nwk*nspin+(spin+1)*nwk] * weights)
-				if nspin == 1:
-					band_occ *= 2
+				#if nspin == 1:
+				#	band_occ *= 2
 				proj_amounts[band][spin] = (defect[band][0][spin], defect[band][1][spin])
 				corr_term = (proj_amounts[band][spin][0] * (hybrid_vbm - bulk_vbm) \
 					+ proj_amounts[band][spin][1] * (hybrid_cbm - bulk_cbm))
